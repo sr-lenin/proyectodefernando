989e23e00ca79b57054d240c7b480eb9d54523d3
sr-lenin <leninalcantarac@gmail.com>
2022-10-10 07:03:12 -0500

    first commit

---
+++ src/give-child.js
@@ -0,0 +1,5 @@
+module.exports= function giveChild( papa, mama, ){
+    return "hijo de " + papa + " y " + mama
+
+}
+
---
+++ src/index.js
@@ -0,0 +1,4 @@
+const giveChild= require("./give-child")
+const sexo = giveChild("wayner ", "haroil ", );
+const sexo2 = giveChild("wayner ", "popola ", );
+console.log(sexo, sexo2);
