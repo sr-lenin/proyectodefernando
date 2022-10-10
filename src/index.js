95412c7349d007d6aa1993cfaa018c537639b36a
sr-lenin <leninalcantarac@gmail.com>
2022-10-10 07:24:13 -0500

    fix harol name

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 const giveChild= require("./give-child")
-const sexo = giveChild("rafael ", "haroil ", );
+const sexo = giveChild("rafael ", "harol ", );
 const sexo2 = giveChild("wayner ", "popola ", );
 console.log(sexo, sexo2);
