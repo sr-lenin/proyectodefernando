b789a13af92578194aa70493858b7a89702fd1cc
sr-lenin <leninalcantarac@gmail.com>
2022-10-10 07:19:36 -0500

    update index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 const giveChild= require("./give-child")
-const sexo = giveChild("wayner ", "haroil ", );
+const sexo = giveChild("rafael ", "haroil ", );
 const sexo2 = giveChild("wayner ", "popola ", );
 console.log(sexo, sexo2);
